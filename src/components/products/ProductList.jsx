ebd285a024f3a492f7728a402c575362030c1961
Gabriel Nicolosi <gabianp@live.com.ar>
2026-06-20 18:54:24 -0300

    Update(ProductList) - deshabilita el Link al productDetail.

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,6 +1,6 @@
 import "../../styles/productList.scss";
 import "../../styles/custom.scss";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import ProductCard from "./ProductCard.jsx";
 const ProductList = ({ productsList }) => {
   // console.log("ProductsList: ", productsList);
@@ -9,9 +9,9 @@ const ProductList = ({ productsList }) => {
       {productsList &&
         productsList.payload.map((prod, prodIndex) => (
           <div key={prodIndex}>
-            <Link to={`/product/${prod.id}`}>
-              <ProductCard product={prod} />
-            </Link>
+            {/* <Link to={`/product/${prod.id}`}> */}
+            <ProductCard product={prod} />
+            {/* </Link> */}
           </div>
         ))}
     </div>
