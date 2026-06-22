0ce6dcbf73893b9dee0e1aa700f2f8863ea8015c
Gabriel Nicolosi <gabianp@live.com.ar>
2026-06-21 21:03:04 -0300

    Update(Vercel) - actualiza archivos de configuración para el deploy en Vercel.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import "./styles/globals.scss";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthProvider.jsx";
@@ -7,10 +8,12 @@ import CartProvider from "./context/cart/CartProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
+    </HelmetProvider>
   </StrictMode>,
 );
